8573369d366538e265eb299ecfda8d2f912e7dd9
Alexis Quintuna <alexisiquintuna@gmail.com>
2022-03-17 13:09:29 -0400

    initial commit

---
+++ src/hooks/use-input.js
@@ -0,0 +1,49 @@
+import { useReducer } from "react";
+
+const initialInputState = {
+  value: "",
+  isTouch: false,
+};
+const inputStateReducer = (state, action) => {
+  if (action.type === "INPUT") {
+    return { value: action.value, isTouched: state.isTouched };
+  }
+  if (action.type === "BLUR") {
+    return { isTouched: true, value: state.value };
+  }
+  if (action.type === "RESET") {
+    return { isTouched: false, value: "" };
+  }
+  return inputStateReducer;
+};
+
+const useInput = (validateValue) => {
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
+
+  const valueIsValid = validateValue(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
+
+  const valueChangeHandler = (event) => {
+    dispatch({ type: "INPUT", value: event.target.value });
+  };
+  const inputBlurHandler = (event) => {
+    dispatch({ type: "BLUR" });
+  };
+  const reset = () => {
+    dispatch({ type: "RESET" });
+  };
+
+  return {
+    value: inputState.value,
+    street: inputState.value,
+    hasError,
+    valueChangeHandler,
+    inputBlurHandler,
+    reset,
+  };
+};
+
+export default useInput;
